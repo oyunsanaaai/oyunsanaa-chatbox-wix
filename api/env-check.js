7cabf07fe136089dbe4413c9fa89a2fccdb82dd0
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-10-16 01:22:02 +0800

    Delete api directory

--- api/env-check.js
+++
@@ -1,5 +0,0 @@
-// /api/env-check.js
-module.exports = (req, res) => {
-  const hasJwt = !!process.env.JWT_SECRET;
-  res.status(200).json({ hasJwt });
-};
--- api/logout.js
+++
@@ -1,6 +0,0 @@
-// api/logout.js
-module.exports = async function handler(req, res) {
-  res.setHeader('Set-Cookie', 'os_auth=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax');
-  res.writeHead(302, { Location: '/' });
-  res.end();
-};
