080ef522b9a0b39caaab8df25755505341825906
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-10-15 23:07:34 +0800

    Delete app directory

--- app/api/guest/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/guest/route.js
-export async function GET() {
-  const target =
-    process.env.WIX_GUEST_URL || "https://oyunsanaa.com/yariltsya"; // хүсвэл солих
-  return Response.redirect(target, 307);
-}
