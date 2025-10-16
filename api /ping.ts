0e22c6abed428f97e2efebb89f7381fc62495068
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-10-16 11:12:51 +0800

    Delete api /ping.ts

--- api /ping.ts
+++
@@ -1,5 +0,0 @@
-// /api/ping.ts
-export const config = { runtime: 'edge' };
-export default function handler() {
-  return new Response(JSON.stringify({ ok:true, ts: Date.now() }), { headers:{'Content-Type':'application/json'} });
-}
